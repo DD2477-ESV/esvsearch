13ad448d89427c50207774a04c36ca7b58baa75a
anderi741 <andreas.j.ericsson@gmail.com>
2022-04-28 11:42:37 +0200

    lite mer för att visa pdfer

--- frontend-react/src/components/ResultPage.js
+++ frontend-react/src/components/ResultPage.js
@@ -4,9 +4,12 @@ import "./../styles/ResultPageStyles.css";
 
 const ResultPage = (props) => {
 	const [docToView, setDocToView] = useState("");
+	const [allDocuements, setAllDocuments] = useState([]);
 	const [documentsToView, setDocumentsToView] = useState([]);
+	const [chosenSorting, setChosenSorting] = useState("relevance");
 
 	useEffect(() => {
+		setAllDocuments(props.documents); // So we can access the original documents later
 		props.documents.length < 20
 			? setDocumentsToView(props.documents)
 			: setDocumentsToView(props.documents.slice(0, 20));
@@ -26,20 +29,89 @@ const ResultPage = (props) => {
 			const currentNumberOfDocuments = documentsToView.length;
 			setDocumentsToView(
 				documentsToView.concat(
-					props.documents.slice(
+					allDocuements.slice(
 						currentNumberOfDocuments,
 						currentNumberOfDocuments + 10
 					)
 				)
 			);
 		} else {
-			setDocumentsToView(props.documents);
+			setDocumentsToView(allDocuements);
 		}
+	};
+
+	/* Functions for handling the sorting of results */
+
+	const compare = (a, b) => {
+		switch (chosenSorting) {
+			case "relevance":
+				console.log("TODO");
+				break;
+			case "date":
+				console.log("TODO");
+			case "title":
+				if (a.title < b.title) {
+					return -1;
+				}
+				if (a.title > b.title) {
+					return 1;
+				}
+				return 0;
+				break;
+			default:
+				return 0;
+		}
+	};
+
+	const sortButtonClicked = (e) => {
+		setChosenSorting(e.target.value);
+		setAllDocuments(allDocuements.sort(compare));
 	};
 
 	// https://github.com/wojtekmaj/react-pdf
 	return (
 		<div className="result-page-container">
+			<div className="container-sort-results">
+				{" "}
+				{/* En box för att sortera resultat */}
+				<h3>Sortera resultat efter</h3>
+				<div className="sort-results-alternatives-box">
+					<button
+						value="relevance"
+						className={
+							chosenSorting === "relevance"
+								? "active-sorting single-result-container-open-button"
+								: "single-result-container-open-button"
+						}
+						onClick={sortButtonClicked}
+					>
+						Relevans
+					</button>
+					<button
+						value="date"
+						className={
+							chosenSorting === "date"
+								? "active-sorting single-result-container-open-button"
+								: "single-result-container-open-button"
+						}
+						onClick={sortButtonClicked}
+					>
+						Datum
+					</button>
+					<button
+						value="title"
+						className={
+							chosenSorting === "title"
+								? "active-sorting single-result-container-open-button"
+								: "single-result-container-open-button"
+						}
+						onClick={sortButtonClicked}
+					>
+						Titel
+					</button>
+				</div>
+			</div>
+
 			<div className="all-results-container">
 				<h3>{"Visar " + documentsToView.length + " dokument"}</h3>
 				{documentsToView.map((doc) => {
@@ -79,7 +151,12 @@ const ResultPage = (props) => {
 			{documentsToView.length === props.documents.length ? (
 				<p>Inga fler dokument att visa...</p>
 			) : (
-				<button onClick={viewMoreClicked} className="single-result-container-open-button">Visa fler dokument</button>
+				<button
+					onClick={viewMoreClicked}
+					className="single-result-container-open-button"
+				>
+					Visa fler dokument
+				</button>
 			)}
 		</div>
 	);
