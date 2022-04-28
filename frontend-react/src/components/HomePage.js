3d0c05980fcba41cad7a98afa4c81034ff46d358
anderi741 <andreas.j.ericsson@gmail.com>
2022-04-28 14:14:15 +0200

    query-test

--- frontend-react/src/components/HomePage.js
+++ frontend-react/src/components/HomePage.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import ResultPage from "./ResultPage";
 import "./../styles/HomePageStyles.css";
 import "./../styles/CheckBoxesStyles.css";
 
 const HomePage = () => {
-	const [results, setResult] = useState([]); // Sätter
+	const [results, setResult] = useState([]);
+	// All input states here
+	const [termsToMatch, setTermsToMatch] = useState("");
+	const [phrase, setPhrase] = useState("");
+	const [termsNotToMatch, setTermsNotToMatch] = useState("");
+	const [dateFrom, setDateFrom] = useState("");
+	const [dateTo, setDateTo] = useState("");
 
 	const handleResponse = (res) => {
 		let pdfsToView = [];
@@ -21,49 +28,106 @@ const HomePage = () => {
 			pdfsToView.push(docInfo);
 		}
 		// Set state to found documents
-		setResult(pdfsToView);
+		//setResult(pdfsToView);
 	};
 
 	const newSearchClicked = (e) => {
 		setResult([]);
 	};
 
-	const searchButtonClicked = (e) => {
-		e.preventDefault();
-		alert("Sökt");
+	const makeRequest = (req) => {
+		const res = await axios.post("localhost:9200/_all/_search", req)
+		console.log(res.data);
+	}
 
-		// Skicka iväg en request med sökningen
-		// vänta på response...
-		// ta emot response....
-		fetch("Example_result.json")
-			.then(function (response) {
-				console.log(response);
-				return response.json();
-			})
-			.then(function (myJson) {
-				console.log(myJson);
-				handleResponse(myJson);
-			});
+	const searchButtonClicked = (e) => {
+		console.log(termsToMatch);
+
+		let jsonObject = {
+			query: {
+				bool: {
+					must: [
+						{
+							match: {
+								text: {
+									query: termsToMatch,
+									operator: "and",
+								},
+							},
+						},
+					],
+				},
+			},
+			fields: ["url", "download_url", "date", "title"],
+			sort: [
+				{
+					date: {
+						order: "desc",
+					},
+				},
+			],
+			highlight: {
+				fields: {
+					text: {},
+				},
+			},
+			_source: false,
+		};
+
+		console.log(jsonObject);
+		makeRequest(jsonObject)
 	};
 
 	return (
 		<div className="homepage-main-container">
 			{results.length !== 0 ? (
 				<div className="result-page-container">
-					<h2>{ "Din sökning gav " + results.length + " resultat"}</h2>
+					<h2>{"Din sökning gav " + results.length + " resultat"}</h2>
 					<ResultPage documents={results} />
-					<button onClick={newSearchClicked} className="new-search-button">Ny sökning</button>
+					<button onClick={newSearchClicked} className="new-search-button">
+						Ny sökning
+					</button>
 				</div>
 			) : (
 				<div className="topnav">
-					<a className="active" href="#home">
+					{/* <a className="active" href="#home">
 						Home
-					</a>
-					<input type="text" placeholder="Sök.." />
-					<link
-						rel="stylesheet"
-						href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-					></link>
+					</a> */}
+					<h1>Ny sökning</h1>
+					<h3>Hitta rapporter</h3>
+					<div className="search-fields">
+						<input
+							type="text"
+							placeholder="Innehållandes dessa ord..."
+							value={termsToMatch}
+							onChange={(e) => setTermsToMatch(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Innehållandes denna fras..."
+							value={phrase}
+							onChange={(e) => setPhrase(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Utan dessa ord..."
+							value={termsNotToMatch}
+							onChange={(e) => setTermsNotToMatch(e.target.value)}
+						/>
+						<p>Mellan följande år</p>
+						<input
+							type="text"
+							placeholder="Från"
+							value={dateFrom}
+							onChange={(e) => setDateFrom(e.target.value)}
+						/>
+						<input
+							type="text"
+							placeholder="Till"
+							value={dateTo}
+							onChange={(e) => setDateTo(e.target.value)}
+						/>
+					</div>
 					<button
 						onClick={searchButtonClicked}
 						className="homepage-search-button"
@@ -75,13 +139,17 @@ const HomePage = () => {
 					<div className="container">
 						<input type="checkbox" /> Arbetsförmedlingen <br />
 						<input type="checkbox" /> Prepositioner <br />
-						<input type="checkbox" /> Annat <br />
+						<input type="checkbox" /> Finansinspektionen <br />
 						<input type="checkbox" /> Annat <br />
 						<input type="checkbox" /> Annat <br />
 						<input type="checkbox" /> Annat <br />
 					</div>
 				</div>
 			)}
+			<link
+				rel="stylesheet"
+				href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+			></link>
 		</div>
 	);
 };
